fe0fae727ce99bd3bbc52c8f4e0d04ad66005063
Damian <steam.panek@gmail.com>
2018-11-18 20:21:42 +0100

    done
    png output and iteration through samples added

--- gan_vanilla.py
+++ gan_vanilla.py
@@ -73,3 +73,69 @@ D_params = [Wxh, bxh, Why, bhy]
 
 params = G_params + D_params
 
+
+
+# Training
+# Reset grad
+
+def reset_grad():
+    for p in params:
+        if p.grad is None:
+            data = p.grad.data
+            p.grad = Variable(data.new().resize_as(data).zero_())
+
+G_solver = optim.Adam(G_params, lr=1e-3)
+D_solver = optim.Adam(D_params, lr=1e-3)
+
+ones_label = Variable(torch.ones(mb_size, 1))
+zeros_labels = Variable(torch.zeros(mb_size, 1))
+
+for it in range(100000):
+
+    # Sample data
+
+    z = Variable(torch.randn(mb_size, Z_dim))
+    X, _ = mnist.train.next_batch(mb_size)
+    X = Variable(torch.from_numpy(X))
+
+    G_sample = G(z)
+    D_real =   D(X)
+    D_fake = D(G_sample)
+
+    D_loss_real = nn.binary_cross_entropy(D_real, ones_label)
+    D_loss_fake = nn.binary_cross_entropy(D_fake, zero_label)
+    D_loss = D_loss_real + D_loss_fake
+
+    D_loss.backward()
+    D_solver.step()
+
+
+    ## Reset grad
+
+
+    if it % 1000 == 0:
+        print('Iter-{}; D-loss: {}; G-loss: {}'.format(it, D_loss.data_numpy(), G_loss.data.numpy()))
+
+        samples = G(z).data.numpy()[:16]
+
+
+        fig = plt.figure(figsize = (4, 4))
+        gs = gridspec.GridSpec(4,4)
+        gs.update(wspace = 0.05, hspace = 0.05)
+
+        for i, sample in enumerate(samples):
+            ax = plt.subplot(gs[i])
+            plt.axis('off')
+            ax.set_xticklabels([])
+            ax.set_yticklabels([])
+            ax.set_aspect('equal')
+            plt.imshow(samples.reshape(28, 28), cmap = 'Greys_r')
+
+        if not os.path.exists('out/'):
+            os.makedirs('out/')
+
+        plt.savefig('out/{}.png'.format(str(c).zfill(3)), bbox_inches='tight')
+        c+=1
+        plt.close(fig)
+
+
